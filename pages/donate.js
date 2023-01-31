c7f67eee1b906bf23f51fabf9a8d9a612390afcf
Chukwudubem Osegbe <lawrenceosegbe97@gmail.com>
2023-01-31 17:47:25 +0100

    add flex box

--- pages/donate.js
+++ pages/donate.js
@@ -18,14 +18,14 @@ export default function Donate() {
           <Header />
         </div>
 
-        <div className="w-full h-[85%] flex items-center justify-center py-20 lg:py-0">
+        <div className="w-full h-[85%] flex flex-col lg:flex-row items-center justify-center py-20 lg:py-0">
           <div className="w-full lg:w-1/2 ">
             <h1 className="text-3xl font-bold text-brandBlue tracking-wider">
               Donation Portal Unavailable. We Are Developing...
             </h1>
           </div>
 
-          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
+          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end mt-10 lg:mt-0">
             <Image
               src={"/brand/RVAIcon.svg"}
               priority
